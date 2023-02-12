dc1a1eb669462245027bfd84c08cbb1d3fe98ed6
Minh Tran <mtran1712@gmail.com>
2023-02-12 15:48:48 -0800

    adding nav styling

--- app/users/layout.jsx
+++ app/users/layout.jsx
@@ -2,11 +2,11 @@ import SideNav from "./sideNav";
 
 const UsersLayout = async ({ children }) => {
   return (
-    <div className="flex">
-      <aside id="default-sidebar" className="bg-gray-50 z-40 w-64 h-screen transition-transform" aria-label="Sidebar">
+    <div>
+      <aside id="default-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
         <SideNav />
       </aside>
-      <div>{children}</div>
+      <div className="p-4 sm:ml-64">{children}</div>
     </div>
   );
 };
